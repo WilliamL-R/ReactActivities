131ff3230fbe72faff1fd8abf7bb14ef5af7a9d5
WilliamL-R <w.loveridge1@users.noreply.github.com>
2025-01-24 17:15:33 +0000

    feat: added missing files to duck demo

--- Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dd2c807d8054db3c75bd5167ab4cf30b5ca4529")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8fdbcecb570a6d7b5774f56e2794aa2c00d689")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
