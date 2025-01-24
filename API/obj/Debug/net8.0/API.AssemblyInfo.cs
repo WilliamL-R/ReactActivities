db42c5371c8bd25348113f94e6b4fb09ff55c6ff
WilliamL-R <w.loveridge1@users.noreply.github.com>
2025-01-24 19:08:04 +0000

    feat: implemented semantic-ui-react and added list / header components

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+131ff3230fbe72faff1fd8abf7bb14ef5af7a9d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed22193932ba17e8f2a7166cf822a059305fea50")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
