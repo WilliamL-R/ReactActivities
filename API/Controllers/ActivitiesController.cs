cc0c4fe3c144485971788e5d6d420ebda6293cad
WilliamL-R <32361704+WilliamL-R@users.noreply.github.com>
2025-01-28 13:09:00 +0000

    refactor: refactored activity controller to get activity using Mediatr

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -19,8 +19,7 @@ namespace API.Controllers
         [HttpGet("{id}")] //api/activities/id
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            //return await _mediator.Activities.FindAsync(id);
-            return null;
+            return await Mediator.Send(new Details.Query{Id = id});
         }
     }
 }
