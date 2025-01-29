aac3d33e2dacaa377858ead7daef2272f32ed515
WilliamL-R <w.loveridge1@gmail.com>
2025-01-29 13:42:28 +0000

    feat: added Delete handler to the API project with MediatR

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a719f36318ae8cbdf893982b5f1436677d1d8db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccbf70e4488f2ba6112ded42aab77dca533b993d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a719f36318ae8cbdf893982b5f1436677d1d8db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccbf70e4488f2ba6112ded42aab77dca533b993d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
