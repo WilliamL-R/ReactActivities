e8b6aa399dc0e5e27c19807fce0652be600f3563
WilliamL-R <w.loveridge1@gmail.com>
2025-01-31 16:03:25 +0000

    feat: allow creation of activity on dashboard

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64db724ed6bddc810fad34bb273a2b32ce79bc6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ab5aff88915d3551c267e523d8ce0547b597b65")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -6,6 +6,7 @@ import { Container } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import {v4 as uuid} from 'uuid'
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -41,9 +42,9 @@ function App() {
 
   function handleCreateOrEditActivity(activity: Activity){
     activity.id ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
-    : setActivities([...activities, activity])
+    : setActivities([...activities, {...activity, id: uuid()}])
     setEditMode(false);
-    selectedActivity(activity);
+    setSelectedActivity(activity);
   }
 
   return (
