36433f1acc90b7f02923cd67c45868d052fda80c
WilliamL-R <w.loveridge1@gmail.com>
2025-01-31 14:38:42 +0000

    feat: allow Activity selection

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+477c7fc54887304f878e67202623a103dee7a678")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+671570f30d9a2d139442d987477eaa6e805072d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -13,9 +13,10 @@ import { Activity } from "../../../app/models/activity";
 
 interface Props {
   activity: Activity;
+  cancelSelectActivity: () => void;
 }
 
-export default function ActivityDetails({ activity }: Props) {
+export default function ActivityDetails({ activity, cancelSelectActivity }: Props) {
   return (
     <Card fluid>
       <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
@@ -29,7 +30,7 @@ export default function ActivityDetails({ activity }: Props) {
       <CardContent extra>
         <ButtonGroup widths="2">
           <Button basic color="blue" content="Edit" />
-          <Button basic color="grey" content="Cancel" />
+          <Button onClick={cancelSelectActivity} basic color="grey" content="Cancel" />
         </ButtonGroup>
       </CardContent>
     </Card>
