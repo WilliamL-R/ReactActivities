4f12045a2aac3b615be108c84eb374f0391798b8
WilliamL-R <w.loveridge1@gmail.com>
2025-01-31 15:27:44 +0000

    feat: added basic form editing and change handlers to Activity form

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+671570f30d9a2d139442d987477eaa6e805072d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64db724ed6bddc810fad34bb273a2b32ce79bc6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
